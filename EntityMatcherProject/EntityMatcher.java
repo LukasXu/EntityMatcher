f52ee196793b0f28b6fc910769e0d2bfa600ecb5
Lukas <utvaz@student.kit.edu>
2023-11-19 14:12:38 +0100

    Adjusted Entity Matcher to implement more conjunctions and to work with adjusted classes

--- EntityMatcherProject/EntityMatcher.java
+++ EntityMatcherProject/EntityMatcher.java
@@ -6,175 +6,175 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import EntityMatcherProject.Rules.DumbRule;
+import EntityMatcherProject.Pair.TableEntryPair;
 import EntityMatcherProject.Rules.Rule;
-import EntityMatcherProject.Rules.SaxRule;
-import EntityMatcherProject.Rules.WordRule;
+import EntityMatcherProject.Rules.RulePairComparitor;
+import EntityMatcherProject.Rules.RulePercentagePair;
+import EntityMatcherProject.SimilarityFunctions.DumbFunction;
+import EntityMatcherProject.SimilarityFunctions.SaxFunction;
+import EntityMatcherProject.SimilarityFunctions.SimilarityFunction;
+import EntityMatcherProject.SimilarityFunctions.WordEditDistance;
 
 public class EntityMatcher {
-    private Map<Integer, Rule> ruleMap = new HashMap<>(); //Mappt regel id auf Regel
-    private List<RulePair> sortedRuleSet;
-    public static List<Pair> ruleSet; //Konjunktion von Lamda
+    private Map<Integer, SimilarityFunction> ruleMap = new HashMap<>(); //Mappt regel id auf Regel
+    private List<RulePercentagePair> sortedRuleSet;
+    public static List<Rule> ruleSet; //Konjunktion von Lamda
 
     static List<TableEntry> exampleTable = Arrays.asList(
-        new TableEntry("dass", "abbbabba", 1991, 2001, 0),
-        new TableEntry("daß", "abbbabba", 1991, 2001, 1),
-        new TableEntry("dann", "cbbbcbbc", 1991, 2001, 2),
-        new TableEntry("dasss", "abbbabba", 1991, 2001, 3),
-        new TableEntry("den", "cbbbcbbc", 1991, 2001, 4)
-        );
+        Parser.findEntry("dass"),
+        Parser.findEntry("daß"),
+        Parser.findEntry("niedrig"),
+        Parser.findEntry("dann"),
+        Parser.findEntry("denn"),
+        Parser.findEntry("den"),
+        Parser.findEntry("niedriger")
+    );
 
-    static List<Pair> posEx = Arrays.asList(
-        new Pair(0, 1),
-        new Pair(0, 3),
-        new Pair(3, 1)
-        );
-
-    static List<Pair> negEx = Arrays.asList(
-        new Pair(0, 2),
-        new Pair(0, 4),
-        new Pair(1, 2),
-        new Pair(1, 4),
-        new Pair(2, 3),
-        new Pair(2, 4),
-        new Pair(3, 4)
-        );
-
+    static List<TableEntryPair> posEx = Arrays.asList(
+        new TableEntryPair(Parser.findEntry("dass"), Parser.findEntry("daß")),
+        new TableEntryPair(Parser.findEntry("niedrig"), Parser.findEntry("niedriger"))
+    );
 
     public EntityMatcher() {
         initRules();
-        if(ruleSet == null) {            
-            ruleSet = generateRuleSet();
+        System.out.println("Example Table: " + exampleTable);
+        if(ruleSet == null) {
+            List<Rule> rules = generateRuleSetSize1();
+            rules.addAll(generateRuleSetSize2());  
+            rules.addAll(generateRuleSetSize3());            
+            ruleSet = rules;
+            //System.out.println("Ruleset: " + ruleSet);
             this.sortedRuleSet = getExampleResult();
+            //System.out.println("Best Rule List: " + sortedRuleSet);
         }   
     }
 
     private void initRules() {
-    	ruleMap.put(0, new WordRule());
-        ruleMap.put(1, new SaxRule());
-        ruleMap.put(2, new DumbRule());
+    	ruleMap.put(0, new WordEditDistance());
+        ruleMap.put(1, new SaxFunction());
+        ruleMap.put(2, new DumbFunction());
     }
 
     //Wendet rule auf table an.
     //Einträge die Matchen werden zusammengefasst
-    public void filter(List<TableEntry> table, Pair rule) {
+    public void filter(List<TableEntry> table, Rule rule) {
+        System.out.println("Zusammenfassen von: " + table);
         List<TableEntry> dummy = new ArrayList<>();
         for(int i = 0; i < table.size(); i++) {
              for (int j = i + 1; j < table.size(); j++) {
-                boolean b = checkRule(table.get(i), table.get(j), rule);
+                boolean b = rule.match(table.get(i), table.get(j));
                 if(b) {
                     dummy.add(table.get(i));
                     dummy.add(table.get(j));
                     table.remove(table.get(i));
-                    table.remove(table.get(j));
+                    table.remove(table.get(j - 1));
                 }
             }
         }
-        System.out.println("Dummy: " + dummy);
-        if(dummy.get(0) == null) {
+        System.out.println("Zusammengefasste Elemente: " + dummy);
+
+        //Falls Elemente zusammengefasst werden, schreibe es in table und die Paare in posEx
+        if(dummy.size() > 0) {
             table.add(dummy.get(0));
+            /* for(int i = 0; i < dummy.size(); i++) {
+                for (int j = i + 1; j < table.size(); j++) {
+                    posEx.add(new TableEntryPair(dummy.get(i), dummy.get(j)));
+                }
+            } */
         }
+        System.out.println("Gefilterte Liste: " + table + "\n");
+        //System.out.println("Neue PosEx " + posEx);
     }
 
-    private boolean checkRule(TableEntry entry1, TableEntry entry2, Pair rule) {
-        Rule r1 = ruleMap.get(rule.getFirst()); 
-        Rule r2 = ruleMap.get(rule.getSecond()); 
-        boolean b1 = r1.match(entry1, entry2);  
-        boolean b2 = r2.match(entry1, entry2); 
-        return  b1 && b2;
-    }
-
-    
-    private List<RulePair> getExampleResult() {
-        List<RulePair> resultList = new ArrayList<>();
+    //Errechnet anhand des Beispiels die beste Regel aus
+    private List<RulePercentagePair> getExampleResult() {
+        List<RulePercentagePair> resultList = new ArrayList<>();
        
         for(int i = 0; i < ruleSet.size(); i++) {
-            Pair pair = ruleSet.get(i);
+            Rule pair = ruleSet.get(i);
             double percentage = matchEntries(pair, exampleTable);
-            resultList.add(new RulePair(pair, percentage));
+            resultList.add(new RulePercentagePair(pair, percentage));
         }
         Collections.sort(resultList, new RulePairComparitor());
         return resultList;
     }
 
-    //Funktioniert nur für Size 2 regeln
-    private double matchEntries(Pair rule, List<TableEntry> table) {
-        List<Pair> pos = new ArrayList<>();
-        List<Pair> neg = new ArrayList<>();
-        Rule r1 = ruleMap.get(rule.getFirst()); 
-        Rule r2 = ruleMap.get(rule.getSecond()); 
+    //Gibt die Erfolgsrate einer Regel angewendet an einer Tabelle an
+    private double matchEntries(Rule rule, List<TableEntry> table) {
+        int matchCounter = 0;
+        int cartesianProductSize = 0;
 
+        //System.out.println("Rule: " + rule.toString());
         for(int i = 0; i < table.size(); i++) {
-             for (int j = i + 1; j < table.size(); j++) {
-                boolean b1 = r1.match(table.get(i), table.get(j));  
-                boolean b2 = r2.match(table.get(i), table.get(j));     
-                Pair entryPair = new Pair(table.get(i).getId(), table.get(j).getId());          
-                if(b1 && b2) {
-                    pos.add(entryPair);
-                } else {
-                    neg.add(entryPair);
+             for (int j = i + 1; j < table.size(); j++) {    
+
+                if(table.get(i)== null || table.get(j) == null) {
+                    continue;
+                }
+                
+                boolean match = rule.match(table.get(i), table.get(j));    
+                TableEntryPair entryPair = new TableEntryPair(table.get(i), table.get(j));    
+                cartesianProductSize++;
+
+                if((match && posEx.contains(entryPair)) || (match == false && posEx.contains(entryPair) == false)) {
+                    //System.out.println("Entry: " + entryPair.toString() + " - Contains " + posEx.contains(entryPair) + " - Match: " + match);
+                    matchCounter++;
                 }
             }
         }
-         
-      /*   //Prints content of pos
-        System.out.println("Pos:");
-        for(Pair p : pos) {
-            System.out.println(p);
-        }  */
+        int denom = cartesianProductSize;
+        double result = (double) (matchCounter) / denom;
 
-       /*  //Prints content of Neg
-        System.out.println("Neg:");
-        for(Pair p : neg) {
-            System.out.println(p);
-        }  */
-
-
-        int m1 = matchPairLists(pos, posEx);
-        int m2 = matchPairLists(neg, negEx);
-/* 
-        System.out.println("M1: " + m1);
-        System.out.println("M2: " + m2); */
-
-        int denom = posEx.size() + negEx.size();
-        double result = (double) (m1 + m2) / denom;
-
-        //System.out.println("Result: " + result + "\n");
+        //System.out.println("Result: " + matchCounter + "/" + denom + " = " + result + "\n");
         return result;
     }
 
-    
-
-    //list1 ausgerechnet, list2 musterlösung
-    private int matchPairLists(List<Pair> list1, List<Pair> list2) {
-        int counter = 0;
-
-        for(int i = 0; i < list1.size(); i++) {
-            for(int j = 0; j < list2.size(); j++) {
-                if(list1.get(i).isSame(list2.get(j))) {
-                    counter++;
-                    continue;
-                }
-            }      
+    private List<Rule> generateRuleSetSize1() {
+        List<Rule> ruleSet = new ArrayList<>();
+        for(int i = 0; i < ruleMap.size(); i++) {         
+            SimilarityFunction r1 = ruleMap.get(i);          
+            List<SimilarityFunction> list = Arrays.asList(r1);
+            Rule rule = new Rule(list);
+            ruleSet.add(rule);   
         }
-        return counter;
+        return ruleSet;
     }
 
     //Generates rule of size 2
     // Ex: lambda1 ^ lambda2
-    private List<Pair> generateRuleSet() {
-        List<Pair> ruleSet = new ArrayList<>();
+    private List<Rule> generateRuleSetSize2() {
+        List<Rule> ruleSet = new ArrayList<>();
         for(int i = 0; i < ruleMap.size(); i++) {
             for (int j = i + 1; j < ruleMap.size(); j++) {
-                ruleSet.add(new Pair(i, j));
-                //System.out.println("Pair " + "(" + i + "," + j + ")");
+                SimilarityFunction r1 = ruleMap.get(i);
+                SimilarityFunction r2 = ruleMap.get(j);
+                List<SimilarityFunction> list = Arrays.asList(r1, r2);
+                Rule rule = new Rule(list);
+                ruleSet.add(rule);
             }
         }
         return ruleSet;
     }
 
-    public List<RulePair> getSortedRuleSet() {
+    private List<Rule> generateRuleSetSize3() {
+        List<Rule> ruleSet = new ArrayList<>();
+        for(int i = 0; i < ruleMap.size(); i++) {
+            for (int j = i + 1; j < ruleMap.size(); j++) {
+                for (int k = j + 1; k < ruleMap.size(); k++) {
+                    SimilarityFunction r1 = ruleMap.get(i);
+                    SimilarityFunction r2 = ruleMap.get(j);
+                    SimilarityFunction r3 = ruleMap.get(k);
+                    List<SimilarityFunction> list = Arrays.asList(r1, r2, r3);
+                    Rule rule = new Rule(list);
+                    ruleSet.add(rule);
+                }
+            }
+        }
+        //System.err.println(ruleSet);
+        return ruleSet;
+    }
+
+    public List<RulePercentagePair> getSortedRuleSet() {
         return sortedRuleSet;
     }
     
